cc45fc558c81012f8d9b9cd990b6cc6fdbbfe32d
coralandbill <347135219@qq.com>
2017-04-20 16:53:34 +0800

    重试逻辑

--- study/src/main/java/com/spring/actual/chapter_04/s01/RetryExecution.java
+++ study/src/main/java/com/spring/actual/chapter_04/s01/RetryExecution.java
@@ -18,7 +18,7 @@ import java.lang.annotation.*;
  2.RetentionPolicy.CLASS —— 这种类型的Annotations编译时被保留,在class文件中存在,但JVM将会忽略
  3.RetentionPolicy.RUNTIME —— 这种类型的Annotations将被JVM保留,所以他们能在运行时被JVM或其他使
 
- Documented 注解表明这个注解应该被 javadoc工具记录. 默认情况下,javadoc是不包括注解的.
+ Documented 注解表明这个注解应该被 javadoc工具记录. 默认情况下,javadoc是不包括注解的 .
  但如果声明注解时指定了 @Documented,则它会被 javadoc 之类的工具处理, 所以注解类型信息也会被包括在生成的文档中.
  */
 @Retention(RetentionPolicy.RUNTIME)//保留时间：运行期
